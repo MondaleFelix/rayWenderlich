5392c6bccabdda04dbf012b32b488016ad11f855
Mondale Felix <mondalefelix@yahoo.com>
2017-12-19 20:06:56 -0800

    grabbed value from the slider

--- Bullseye/Bullseye/ViewController.swift
+++ Bullseye/Bullseye/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var currentValue: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,10 +22,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sliderMoved(_ slider: UISlider){
+        print("The value of the slider is now: \(slider.value)" )
+        currentValue = lroundf(slider.value)
+        
+    }
+    
     @IBAction func showAlert(){
         
+        let message = "The Value of the slider is: \(currentValue)"
+        
+        
 //      Creates Alert Modal
-        let alert = UIAlertController(title: "Hello World", message: "This is my first alert", preferredStyle: .alert)
+        let alert = UIAlertController(title: "Hello World", message: message, preferredStyle: .alert)
         
 //      Creates the dissmiss button
         let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
