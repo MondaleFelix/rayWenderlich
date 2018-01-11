281cc550ee6574f53e5c1ea79ad9fd93c04f0159
Mondale Felix <mondalefelix@yahoo.com>
2018-01-11 10:21:25 -0800

    added startNewRounds function

--- Bullseye/Bullseye/ViewController.swift
+++ Bullseye/Bullseye/ViewController.swift
@@ -10,27 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var currentValue: Int = 0
+    var currentValue: Int = 50
+    @IBOutlet weak var slider: UISlider!
+    var targetValue: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        currentValue = lroundf(slider.value)
+        startNewRounds()
     }
 
+    func startNewRounds() {
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
     @IBAction func sliderMoved(_ slider: UISlider){
-        print("The value of the slider is now: \(slider.value)" )
+        print("The value of the slider is now: \(slider.value)")
         currentValue = lroundf(slider.value)
         
     }
     
     @IBAction func showAlert(){
         
-        let message = "The Value of the slider is: \(currentValue)"
+        let message = "The Value of the slider is: \(currentValue)" +
+        "\n The target value is: \(targetValue)"
         
         
 //      Creates Alert Modal
@@ -44,6 +54,8 @@ class ViewController: UIViewController {
         
 //      Shows alert when user presses button
         present(alert, animated: true, completion: nil)
+        
+        startNewRounds()
     }
 
     
