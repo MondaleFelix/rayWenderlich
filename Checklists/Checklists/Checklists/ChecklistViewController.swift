eceb8b5c4d5bb7f1455ba64be89ea2e70fa716a5
Mondale Felix <mondalefelix@yahoo.com>
2018-01-16 21:34:44 -0800

    refactored and added toggle method

--- Checklists/Checklists/Checklists/ChecklistViewController.swift
+++ Checklists/Checklists/Checklists/ChecklistViewController.swift
@@ -80,9 +80,9 @@ class ChecklistViewController: UITableViewController {
         if let cell = tableView.cellForRow(at: indexPath){
             
             let item = items[indexPath.row]
-            item.checked = !item.checked
+            item.toggleChecked()
             
-            configureCheckmark(for: cell, at: indexPath)
+            configureCheckmark(for: cell, with: item)
         //deselects row after uses presses
             tableView.deselectRow(at: indexPath, animated: true)
         }
@@ -90,19 +90,22 @@ class ChecklistViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
-        let label = cell.viewWithTag(1000) as! UILabel
         
         let item = items[indexPath.row]
-        label.text = item.text
         
-        
-        configureCheckmark(for: cell, at: indexPath)
+        configureText(for: cell, with: item)
+        configureCheckmark(for: cell, with: item)
         return cell
     }
 
-    func configureCheckmark(for cell : UITableViewCell, at indexPath: IndexPath){
+    func configureText(for cell: UITableViewCell, with item: ChecklistItem){
+        let label = cell.viewWithTag(1000) as! UILabel
+        label.text = item.text
+
+    }
+    
+    func configureCheckmark(for cell : UITableViewCell, with item: ChecklistItem){
         
-        let item = items[indexPath.row]
         if item.checked{
             cell.accessoryType = .checkmark
         } else {
