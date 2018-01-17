6a953e0fb57d448bcde013e2791ee92f2fd50ca8
Mondale Felix <mondalefelix@yahoo.com>
2018-01-16 16:51:35 -0800

    refactored

--- Checklists/Checklists/Checklists/ChecklistViewController.swift
+++ Checklists/Checklists/Checklists/ChecklistViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ChecklistViewController: UITableViewController {
 
+    let row0text = "Walk the Dog"
+    let row1text = "Brush Teeth"
+    let row2text = "Learn IOS development"
+    let row3text = "Soccer Practice"
+    let row4text = "Eat Ice cream"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -40,6 +46,18 @@ class ChecklistViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
         let label = cell.viewWithTag(1000) as! UILabel
+        
+        if indexPath.row == 0 {
+            label.text = row0text
+        }  else if indexPath.row == 1{
+            label.text = row1text
+        }  else if indexPath.row == 2{
+            label.text = row2text
+        }  else if indexPath.row == 3{
+            label.text = row3text
+        }  else if indexPath.row == 4{
+            label.text = row4text
+        }
         return cell
     }
 
