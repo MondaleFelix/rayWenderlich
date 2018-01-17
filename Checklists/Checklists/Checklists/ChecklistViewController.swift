5d6a8988bf7c12ef95e108fafd905cc520dcebf6
Mondale Felix <mondalefelix@yahoo.com>
2018-01-16 17:13:01 -0800

    refactored accessoryType

--- Checklists/Checklists/Checklists/ChecklistViewController.swift
+++ Checklists/Checklists/Checklists/ChecklistViewController.swift
@@ -16,6 +16,12 @@ class ChecklistViewController: UITableViewController {
     let row3text = "Soccer Practice"
     let row4text = "Eat Ice cream"
     
+    var row0Checked = false
+    var row1Checked = false
+    var row2Checked = false
+    var row3Checked = false
+    var row4Checked = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -33,11 +39,24 @@ class ChecklistViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //returns a cell that is found by row address and adds/removes checkmark
         if let cell = tableView.cellForRow(at: indexPath){
-            if cell.accessoryType == .none{
-                cell.accessoryType = .checkmark
-            } else {
-                cell.accessoryType = .none
+            
+            if indexPath.row == 0{
+                row0Checked = !row0Checked
             }
+            if indexPath.row == 1{
+                row1Checked = !row1Checked
+            }
+            if indexPath.row == 2{
+                row2Checked = !row2Checked
+            }
+            if indexPath.row == 3{
+                row3Checked = !row3Checked
+            }
+            if indexPath.row == 4{
+                row4Checked = !row4Checked
+            }
+            
+            configureCheckmark(for: cell, at: indexPath)
         //deselects row after uses presses
             tableView.deselectRow(at: indexPath, animated: true)
         }
@@ -58,8 +77,32 @@ class ChecklistViewController: UITableViewController {
         }  else if indexPath.row == 4{
             label.text = row4text
         }
+        configureCheckmark(for: cell, at: indexPath)
         return cell
     }
 
+    func configureCheckmark(for cell : UITableViewCell, at indexPath: IndexPath){
+        
+        var isChecked = false
+        if indexPath.row == 0 {
+            isChecked = row0Checked
+        } else if indexPath.row == 1 {
+            isChecked = row1Checked
+        } else if indexPath.row == 2 {
+            isChecked = row2Checked
+        } else if indexPath.row == 3 {
+            isChecked = row3Checked
+        } else if indexPath.row == 4 {
+            isChecked = row4Checked
+        }
+        
+        if isChecked {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+            
+        }
+    }
+    
 }
 
